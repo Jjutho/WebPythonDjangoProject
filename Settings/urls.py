f60aa957518eaebab2b5b696383b3f83233237a0
Julian Hoff <julian@jut-so.de>
2023-01-29 17:59:44 +0100

    fixed download url

--- Settings/urls.py
+++ Settings/urls.py
@@ -12,7 +12,4 @@ urlpatterns = [
     path('games/', include('Games.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('shoppingcart/', include('Shoppingcart.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
